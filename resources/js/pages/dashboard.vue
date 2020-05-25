aa676af22fb66c9a2ebc01eb5067f58498a81134
Ali.Seivani <peshto@gmail.com>
2020-05-26 03:25:17 +0600

    changes

--- resources/js/pages/dashboard.vue
+++ resources/js/pages/dashboard.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
     <base-header type="gradient-success" class="pb-8 pt-9 pt-md-9">
-      <!-- Card stats -->
       <div class="row">
         <div class="col-xl-3 col-lg-6">
           <stats-card title="Total traffic"
@@ -62,70 +61,7 @@
         </div>
       </div>
     </base-header>
-    <!--Charts-->
     <div class="container-fluid mt-n6">
-      <div class="row">
-        <div class="col-xl-8 mb-5 mb-xl-0">
-          <card type="default" header-classes="bg-transparent">
-            <div slot="header" class="row align-items-center">
-              <div class="col">
-                <p class="text-light text-uppercase ls-1 mb-1">Overview</p>
-                <h5 class="h5 text-white mb-0">Sales value</h5>
-              </div>
-              <div class="col">
-                <ul class="nav nav-pills justify-content-end">
-                  <li class="nav-item mr-2 mr-md-0">
-                    <a class="nav-link py-2 px-3"
-                       href="#"
-                       :class="{active: bigLineChart.activeIndex === 0}"
-                       @click.prevent="initBigChart(0)">
-                      <span class="d-none d-md-block">Month</span>
-                      <span class="d-md-none">M</span>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link py-2 px-3"
-                       href="#"
-                       :class="{active: bigLineChart.activeIndex === 1}"
-                       @click.prevent="initBigChart(1)">
-                      <span class="d-none d-md-block">Week</span>
-                      <span class="d-md-none">W</span>
-                    </a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <line-chart
-              :height="350"
-              ref="bigChart"
-              :chart-data="bigLineChart.chartData"
-              :extra-options="bigLineChart.extraOptions"
-            >
-            </line-chart>
-
-          </card>
-        </div>
-
-        <div class="col-xl-4">
-          <card header-classes="bg-transparent">
-            <div slot="header" class="row align-items-center">
-              <div class="col">
-                <p class="text-uppercase text-muted ls-1 mb-1">{{ $t('you_are_logged_in') }}</p>
-                <h5 class="mb-0">Total orders</h5>
-              </div>
-            </div>
-
-            <bar-chart
-              :height="350"
-              ref="barChart"
-              :chart-data="redBarChart.chartData"
-            >
-            </bar-chart>
-          </card>
-        </div>
-      </div>
-      <!-- End charts-->
-
       <!--Tables-->
       <div class="row mt-5">
         <div class="col-xl-8 mb-5 mb-xl-0">
@@ -175,46 +111,14 @@ export default {
   },
   data () {
     return {
-      bigLineChart: {
-        allData: [
-          [0, 20, 10, 30, 15, 40, 20, 60, 60],
-          [0, 20, 5, 25, 10, 30, 15, 40, 40]
-        ],
-        activeIndex: 0,
-        chartData: {
-          datasets: [],
-          labels: []
-        },
-        extraOptions: chartConfigs.blueChartOptions,
-      },
-      redBarChart: {
-        chartData: {
-          labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-          datasets: [{
-            label: 'Sales',
-            data: [25, 20, 30, 22, 17, 29]
-          }]
-        }
-      }
+
     }
   },
   methods: {
-    initBigChart (index) {
-      let chartData = {
-        datasets: [
-          {
-            label: 'Performance',
-            data: this.bigLineChart.allData[index]
-          }
-        ],
-        labels: ['May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
-      }
-      this.bigLineChart.chartData = chartData
-      this.bigLineChart.activeIndex = index
-    }
+
   },
   mounted () {
-    this.initBigChart(0)
+
   },
   computed: mapGetters({
     authenticated: 'auth/check'
